a2cb0d248f0ea544c4d047dd77d8d167299566e3
Ville <ville.helin@tuni.fi>
2021-12-17 19:36:32 +0200

    date input field and unix time convert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bitcoin-application';
+  startUnix: number;
+  endUnix: number;
+
+  searchButton(startdate, enddate){
+    console.log("startdate is", startdate);
+    console.log("enddate is", enddate);
+
+    this.dateToUnixTime(startdate, enddate)
+  }
+
+  dateToUnixTime(startdate, enddate) {
+    this.startUnix = new Date(startdate + ' 02:00:00').getTime() / 1000;  // Adding two hours to date to ensure getting data from that day
+    this.endUnix = new Date(enddate + ' 02:00:00').getTime() / 1000;  // Adding two hours to date to ensure getting data from that day
+    console.log(this.startUnix)
+    console.log(this.endUnix)
+  }
 }
