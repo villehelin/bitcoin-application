724f1d88edf057980ce28f9111fc1686a70d487e
Ville <ville.helin@tuni.fi>
2021-12-22 17:13:25 +0200

    code for highest trading volume for given range

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { runInThisContext } from 'vm';
 import { BitcoinService } from './bitcoin.service';
 
 @Component({
@@ -13,6 +14,10 @@ export class AppComponent {
   bitcoin: any[];
   counter: number;
   bearish: number;
+  maxBearish: number;
+  highestVolume: number;
+  highestTradingDate: number;
+  highestTradingDateString: string;
 
   constructor(private bitcoinService: BitcoinService) {}
 
@@ -33,23 +38,35 @@ export class AppComponent {
 
     this.counter = 0;
     this.bearish = 0;
+    this.maxBearish = 0;
 
     // How many days is the longest bearish (downward) trend within a given date range?
-    for (let i = 0; i < this.bitcoin.prices.length; i++) {
-      if (this.bitcoin.prices[i+1][1] > this.bitcoin.prices[i][1]) {
-        if (this.bearish < this.counter) {
-          this.bearish = this.counter;
-          this.counter = 0;
+    for (let i = 0; i < this.bitcoin.prices.length - 1; i++) {
+      if (this.bitcoin.prices[i + 1][1] > this.bitcoin.prices[i][1]) {
+        if (this.maxBearish < this.bearish) {
+          this.maxBearish = this.bearish; 
         }
+        this.bearish = 0;
       }
-      else if (this.bitcoin.prices[i+1][1] <= this.bitcoin.prices[i][1]) {
-        this.counter++;
+      else if (this.bitcoin.prices[i + 1][1] < this.bitcoin.prices[i][1]) {
+        this.bearish++;
       }
     }
 
-    
+    this.highestTradingDate = 0;
+    this.highestVolume = 0;
 
-   
+
+    // Which date within a given date range had the highest trading volume?
+    for (let i = 0; i < this.bitcoin.total_volumes.length; i++) {
+      if (this.bitcoin.total_volumes[i][1] > this.bitcoin.total_volumes[this.highestTradingDate][1]) {
+        this.highestTradingDate = i;
+        this.highestVolume = this.bitcoin.total_volumes[i][1]; 
+      }
+    }
+
+    const date = new Date(this.bitcoin.total_volumes[this.highestTradingDate][0]);
+    this.highestTradingDateString = date.toLocaleDateString("fi-FI");
   }
 
   dateToUnixTime(startdate, enddate) {
--- src/app/bitcoin.service.ts
+++ src/app/bitcoin.service.ts
@@ -9,6 +9,6 @@ export class BitcoinService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    return this.http.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1577836800&to=1609376400');
+    return this.http.get('https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=1577836800&to=1609376400');
   }
 }
